b2d5275f13aec9c2a06722d8fb2b174756dad069
MPedrosaB <mpedrosab@gmail.com>
2020-05-09 18:47:29 +0200

    Adding layer to viewer

--- fdtd/1d/src/fdtd/viewer.py
+++ fdtd/1d/src/fdtd/viewer.py
@@ -3,7 +3,7 @@ import matplotlib.animation as animation
 
 class Animator:
 
-    def __init__(self, mesh, probe,analytical=None):
+    def __init__(self, mesh, probe,analytical=None, layer=None):
             
         ids = probe["indices"]
         gridE = mesh.pos[ids[0]:ids[1]]
@@ -21,6 +21,8 @@ class Animator:
         line1,    = ax1.plot([], [], '-', markersize=1)
         timeText1 = ax1.text(0.02, 0.95, '', transform=ax1.transAxes)
 
+        if layer is not None:
+            ax1.fill_betweenx([-1,1], layer.coords[0], layer.coords[-1],color='orange',alpha=0.50)
         def init():
             line1.set_data([ ], [ ])
             timeText1.set_text('')
